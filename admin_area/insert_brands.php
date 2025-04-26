d6e1816f084615b28077bd3ba6ec649429ce3979
shahriarnaiem-rafi <shahriarnaiemrafi33@gmail.com>
2025-04-26 23:09:27 +0600

    brand and categories added

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,14 +1,35 @@
+<?php
+include('../includes/connect.php');
+if (isset($_POST['insert_brand'])) {
+    $brand_title = $_POST['brand_titile'];
+    // data base ar sob data dekhe
+    $select_query = "Select * from brands where brand_title='$brand_title'";
+    $result_select = mysqli_query($con, $select_query);
+
+    // jodi 1 bar already thake r thak be na
+    $number = mysqli_num_rows($result_select);
+    if ($number > 0) {
+        echo "<script>alert('brand already added before')</script>";
+    } else {
+        $insert_query = "insert into brands (brand_title) values('$brand_title')";
+        $result = mysqli_query($con, $insert_query);
+        if ($result) {
+            echo "<script>alert('brand added succesfulli')</script>";
+
+        }
+    }
+}
+?>
 <form action="" method="post" class="mb-2">
     <div class="input-group w-90 mb-3 ">
         <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"> </i></span>
-        <input type="text" class="form-control" name="branch_title" placeholder="Insert Branch" aria-label="Branch"
+        <input type="text" class="form-control" name="brand_titile" placeholder="Insert Brand" aria-label="Brand"
             aria-describedby="basic-addon1">
     </div>
     <div class="input-group  mb-2">
 
-        <button class="bg-info p-3 border-0 my-3">
-            Insert Branch
-        </button>
+        <input type="submit" class="bg-info border-0 p-2 m-2" name="insert_brand" value="Insert Brand">
+
     </div>
 
 </form>
--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,5 +1,26 @@
 <?php
-include("../includes/connect.php")
+include("../includes/connect.php");
+if (isset($_POST['insert_cat'])) {
+    $category_title = $_POST['cat_title'];
+    // select data from database
+    $select_query = "Select * from categories where category_title='$category_title'";
+    $result_select = mysqli_query($con, $select_query);
+
+    // jodi data akbar thake r thakbe na
+    $number = mysqli_num_rows($result_select);
+    if ($number > 0) {
+        echo "<script>alert('categories already added ')</script>";
+    } else {
+
+
+        $insert_query = "insert into categories (category_title) values('$category_title')";
+        $result = mysqli_query($con, $insert_query);
+        if ($result) {
+            echo "<script>alert('categories added succesfulli')</script>";
+        }
+    }
+
+}
 ?>
 <form action="" method="post" class="mb-2">
     <div class="input-group w-90 mb-3 ">
@@ -8,10 +29,10 @@ include("../includes/connect.php")
             aria-describedby="basic-addon1">
     </div>
     <div class="input-group  mb-2">
-
-        <button class="bg-info p-3 border-0 my-3">
+        <input type="submit" class="bg-info border-0 p-2 m-2" name="insert_cat" value="Insert Categories">
+        <!-- <button class="bg-info p-3 border-0 my-3">
             Insert Categories
-        </button>
+        </button> -->
     </div>
 
 </form>
